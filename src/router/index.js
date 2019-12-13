a01b64facf4cab4fbb6c1023d8284de3dbe38dde
13742 <1374214273@qq.com>
2019-12-13 23:21:52 +0800

    添加了侧边栏和页头

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import BaiduMap from 'vue-baidu-map'
 import HelloWorld from '@/components/HelloWorld'
+import BaiduMap from 'vue-baidu-map'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import leftNavBar from '../components/leftNavBar'
 
 Vue.use(Router)
+Vue.use(leftNavBar)
+Vue.use(BootstrapVue)
 Vue.use(BaiduMap, {
   ak: 'GBGNbNyCEePZ3WncU8gaQQpcowYZUIvz'
 })
+
 
 export default new Router({
   routes: [
@@ -14,6 +21,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+
+    {
+      path: '/',
+      name: 'leftNavBar',
+      component: leftNavBar
     }
   ]
 })
